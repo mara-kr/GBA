d82c4237765a0d97a5c4f540d714dffa29082ff7
Neil D. Ryan <soctar@gmail.com>
2016-09-09 21:54:27 -0400

    Added core from "random guy on the internet" Makefile has a couple decent goals:     test_alu: tests the ALU (verilog testbench for VHDL code!)     test_core: Tests the core, uses verilog top module to make sure it     stops     clean: cleans. Builds in build directory, maybe rethink it a bit
    Currently pretty untested, but the core simulates & gives bus logs.
    Deciding on memory interface between core and memory controller is next
    step
    
    Also, modified ClockAndResetGenerator to operate on seconds (so
    everything agrees - verilog #1 is in seconds when blended with VHDL
    apparently)

---
+++ ARM7TDMI/ALU/ARMALUTestTop.vhd
@@ -0,0 +1,134 @@
+--****************************************************************************************************
+-- ALU tester top entity for ARM core
+-- Designed by Ruslan Lepetenok
+-- Modified 09.11.2002
+--****************************************************************************************************
+library	IEEE;
+use IEEE.std_logic_1164.all;
+
+entity ARMALUTestTop is
+end ARMALUTestTop;
+
+architecture STRUCT of ARMALUTestTop is
+
+component ClockAndResetGenerator is port (
+	                                      nRESET  : out std_logic;
+						                  CLK     : out std_logic
+						                  );
+end component;
+
+component ALUTesterSim is port (
+	                    nRESET  : in std_logic;
+						CLK    : in std_logic;
+
+						ADataOut   : out  std_logic_vector(31 downto 0);
+						BDataOut   : out  std_logic_vector(31 downto 0);
+						InvA	   : out  std_logic;
+						InvB	   : out  std_logic;
+						PassA	   : out  std_logic;
+						PassB	   : out  std_logic;	-- MOV/MVN operations
+						-- Logic operations
+						AND_Op	   : out  std_logic;
+						ORR_Op	   : out  std_logic;
+						EOR_Op	   : out  std_logic;
+						-- Tester flag outputs
+						CFlagOut   : out  std_logic;
+						CFlagUse   : out  std_logic  -- ADC/SBC/RSC instructions
+						     );
+end component;
+
+component ALU is port (
+	                    ADataIn    : in  std_logic_vector(31 downto 0);
+						BDataIn    : in  std_logic_vector(31 downto 0);
+						DataOut    : out std_logic_vector(31 downto 0);
+						InvA	   : in  std_logic;
+						InvB	   : in  std_logic;
+						PassA	   : in  std_logic;
+						PassB	   : in  std_logic;	-- MOV/MVN operations
+						-- Logic operations
+						AND_Op	   : in  std_logic;
+						ORR_Op	   : in  std_logic;
+						EOR_Op	   : in  std_logic;
+						-- Flag inputs
+						CFlagIn	   : in  std_logic;
+						CFlagUse   : in  std_logic; -- ADC/SBC/RSC instructions
+						-- Flag outputs
+						CFlagOut    : out  std_logic;
+						VFlagOut    : out  std_logic;
+						NFlagOut    : out  std_logic;
+						ZFlagOut    : out  std_logic
+				    );
+end component;
+
+
+-- Signal declarations
+signal nRESET    : std_logic := '0';
+signal CLK      : std_logic := '0';
+
+signal ADataBus : std_logic_vector(31 downto 0) := (others => '0');
+signal BDataBus : std_logic_vector(31 downto 0) := (others => '0');
+signal InvA	    : std_logic := '0';
+signal InvB	    : std_logic := '0';
+signal PassA	: std_logic := '0';
+signal PassB	: std_logic := '0';
+
+signal AND_Op	: std_logic := '0';
+signal ORR_Op	: std_logic := '0';
+signal EOR_Op   : std_logic := '0';
+
+signal CFlag	: std_logic := '0';
+signal CFlagUse : std_logic := '0';
+
+
+begin
+
+CLK_And_nRESET:component ClockAndResetGenerator port map (
+	                                      nRESET  => nRESET,
+						                  CLK    => CLK
+						                  );
+
+
+Tester:component ALUTesterSim  port map (
+	                    nRESET   => nRESET,
+						CLK     => CLK,
+
+						ADataOut => ADataBus,
+						BDataOut => BDataBus,
+						InvA	 => InvA,
+						InvB	 => InvB,
+						PassA	 => PassA,
+						PassB	 => PassB,
+						-- Logic operations
+						AND_Op	 => AND_Op,
+						ORR_Op	 => ORR_Op,
+						EOR_Op	 => EOR_Op,
+						-- Flag inputs
+						CFlagOut => CFlag,
+						CFlagUse => CFlagUse
+						     );
+
+
+ALUUnderTest:component ALU port map(
+	                    ADataIn => ADataBus,
+						BDataIn => BDataBus,
+						DataOut => open,
+						InvA	=> InvA,
+						InvB	=> InvB,
+						PassA	 => PassA,
+						PassB	=> PassB,
+						-- Logic operations
+						AND_Op	=> AND_Op,
+						ORR_Op	=> ORR_Op,
+						EOR_Op	=> EOR_Op,
+						-- Flag inputs
+						CFlagIn	=> CFlag,
+						CFlagUse => CFlagUse,
+						-- Flag outputs
+						CFlagOut => open,
+						VFlagOut => open,
+						NFlagOut => open,
+						ZFlagOut => open
+				    );
+
+end STRUCT;
+
--- ARM7TDMI/RAM32B.vhd
+++ ARM7TDMI/RAM32B.vhd
@@ -1,48 +1,48 @@
--**************************************************************************************************** 
--- 32-bit RAM(behavioural) for ARM core simualtion 
--- Designed by Ruslan Lepetenok 
--- Modified 01.01.2003 
---**************************************************************************************************** 
- 
-library IEEE; 
-use IEEE.std_logic_1164.all; 
-use IEEE.std_logic_unsigned.all; 
- 
-use Work.ARMSMSSPackage.all; 
- 
-entity RAM32B is generic(RAMSize : positive := 128); 
-	              port( 
-		     		   -- Global control signals 
-        			   CLK       : in  std_logic; 
-					   CLKEN     : in  std_logic; 
-  	                   -- Address and data 
-					   Address   : in  std_logic_vector(LOG2(RAMSize/4)-1 downto 0); 
-                       RAMSel	 : in  std_logic; 
-					   ByteWE	 : in  std_logic_vector(3 downto 0); 
-		               DataIn    : in  std_logic_vector(31 downto 0); 
-		               DataOut   : out std_logic_vector(31 downto 0) 
-					   ); 
-end RAM32B; 
- 
-architecture Beh of RAM32B is 
-type RAM32BFileType is array(0 to RAMSize/4-1) of std_logic_vector(DataIn'range); 
-signal RAM32BFile : RAM32BFileType := (others => x"00000000");  
- 
-begin 
- 
-DataWrite:process(CLK) 
-begin 
-if CLK='1' and CLK'event then            -- Clock 
- if CLKEN='1' then                       -- Clock enable  
-  for i in ByteWE'range loop 
-	 if ByteWE(i)='1' and RAMSel='1' then   
-	  RAM32BFile(CONV_INTEGER(Address))((i+1)*8-1 downto i*8) <= DataIn((i+1)*8-1 downto i*8);	 
-   	 end if; 
-	end loop; 
-  end if; 
-end if; 
-end process;	 
- 
-DataOut <= RAM32BFile(CONV_INTEGER(Address)); 
- 
-end Beh; 
+--****************************************************************************************************
+-- 32-bit RAM(behavioural) for ARM core simualtion
+-- Designed by Ruslan Lepetenok
+-- Modified 01.01.2003
+--****************************************************************************************************
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+use Work.ARMSMSSPackage.all;
+
+entity RAM32B is generic(RAMSize : positive := 128);
+	              port(
+		     		   -- Global control signals
+        			   CLK       : in  std_logic;
+					   CLKEN     : in  std_logic;
+  	                   -- Address and data
+					   Address   : in  std_logic_vector(LOG2(RAMSize/4)-1 downto 0);
+                       RAMSel	 : in  std_logic;
+					   ByteWE	 : in  std_logic_vector(3 downto 0);
+		               DataIn    : in  std_logic_vector(31 downto 0);
+		               DataOut   : out std_logic_vector(31 downto 0)
+					   );
+end RAM32B;
+
+architecture Beh of RAM32B is
+type RAM32BFileType is array(0 to RAMSize/4-1) of std_logic_vector(DataIn'range);
+signal RAM32BFile : RAM32BFileType := (others => x"00000000");
+
+begin
+
+DataWrite:process(CLK)
+begin
+if CLK='1' and CLK'event then            -- Clock
+ if CLKEN='1' then                       -- Clock enable
+  for i in ByteWE'range loop
+	 if ByteWE(i)='1' and RAMSel='1' then
+	  RAM32BFile(CONV_INTEGER(Address))((i+1)*8-1 downto i*8) <= DataIn((i+1)*8-1 downto i*8);
+   	 end if;
+	end loop;
+  end if;
+end if;
+end process;
+
+DataOut <= RAM32BFile(CONV_INTEGER(Address));
+
+end Beh;
