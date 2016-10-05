36365a817b587395da697ab79f0e4e8b7e091f4b
Neil D. Ryan <soctar@gmail.com>
2016-10-05 10:49:57 -0400

    Changed makefile & TB for fixing LR bug

--- cpu/core_tb.sv
+++ cpu/core_tb.sv
@@ -47,7 +47,7 @@ module core_tb;
     integer cyc_count;
     /* So the simulation stops */
     initial begin
-        #500 $finish;
+        #84 $finish;
     end
 
     always_ff @(posedge clk) begin
