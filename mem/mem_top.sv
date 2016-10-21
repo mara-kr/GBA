ad0df93b8634b9dafc112561adfc7c0f05bcf600
Neil D. Ryan <soctar@gmail.com>
2016-10-20 22:41:24 -0400

    Added block comment at top and todos. Changed muxing for memory to be on last cycle. Added register module w/ clr > en.

--- mem/mem_top.sv
+++ mem/mem_top.sv
@@ -1,4 +1,25 @@
-// TODO Test BRAM
+/* mem_top.sv
+ * Top module for the GBA memory interface. Consists of 5 BRAM regions:
+ * System ROM, CPU Internal RAM, OAM, VRAM, and Palette RAM.
+ *
+ * "bus" I/O signals are for the CPU/DMA bus, "gfx" I/O signals are for
+ * the graphics pipeline (read-only). Either port of the memory
+ * controller can assert "pause" on long accesses - when this occurs,
+ * whatever system is driving the memory controller's relevant input
+ * signals should hold those signals constant.
+ *
+ * "size" refers to the size of the memory write, and assumes that the
+ * data is passed to the memory controller in little endian format.
+ *
+ * Neil Ryan, <nryan@andrew.cmu.edu>
+ */
+
+// TODO Test Consecutive Reads (DMA timing)
+// TODO Test Consecutive Writes (DMA timing)
+// TODO Test Reading from Graphics & CPU at once
+// TODO Test different size writes
+// TODO Test Pause on reads
+
 `default_nettype none
 `include "core_tb_defines.vh"
 
@@ -21,53 +42,51 @@ module mem_top(
     );
 
     /* Single cycle latency for writes */
-    logic [31:0] bus_addr_lat1, bus_mem_addr;
+    logic [31:0] bus_addr_lat1, bus_mem_addr, gfx_addr_lat1;
     logic  [1:0] bus_size_lat1;
     logic        bus_write_lat1;
 
     // Could add more pauses for memory regions, this is needed
     // because of the CPU's write format
-    assign bus_pause = bus_write_lat1;
     assign gfx_pause = 1'b0;
+
     // Use delayed memory address on writes
     assign bus_mem_addr = (bus_write_lat1) ? bus_addr_lat1 : bus_addr;
 
     // Registers to delay write signals
-    always_ff @(posedge clock, posedge reset) begin
-        if (reset) begin
-            bus_addr_lat1 <= 32'd0;
-            bus_write_lat1 <= 1'b0;
-            bus_size_lat1 <= 2'b0;
-        end else begin
-            bus_addr_lat1 <= bus_addr;
-            bus_write_lat1 <= bus_write;
-            bus_size_lat1 <= bus_size;
-        end
-    end
+    register #(32) baddr (.clock, .reset, .en(1'b0), .clr(1'b0),
+                          .D(bus_addr), .Q(bus_addr_lat1));
+    register #(1) bwrite (.clock, .reset, .en(1'b0), .clr(1'b0),
+                          .D(bus_write), .Q(bus_write_lat1));
+    register #(2) bsize (.clock, .reset, .en(1'b0), .clr(1'b0),
+                         .D(bus_size), .Q(bus_size_lat1));
+    // Pauses due to writes, could be extended
+    register #(1) wpause (.clock, .reset, .en(1'b0), .clr(1'b0),
+                         .D(bus_write & ~bus_pause), .Q(bus_pause));
 
     logic [31:0] bus_system_addr, bus_system_rdata, gfx_system_addr;
     logic [31:0] gfx_system_rdata;
-    logic        bus_system_valid, gfx_system_valid;
+    logic        bus_system_read, gfx_system_read;
 
     logic [31:0] bus_intern_addr, bus_intern_rdata, gfx_intern_addr;
     logic [31:0] gfx_intern_rdata;
     logic  [3:0] bus_intern_we;
-    logic        bus_intern_valid, gfx_intern_valid;
+    logic        bus_intern_read, bus_intern_write, gfx_intern_read;
 
     logic [31:0] bus_vram_addr, bus_vram_rdata, gfx_vram_addr;
     logic [31:0] gfx_vram_rdata;
     logic  [3:0] bus_vram_we;
-    logic        bus_vram_valid, gfx_vram_valid;
+    logic        bus_vram_read, bus_vram_write, gfx_vram_read;
 
     logic [31:0] bus_pallete_addr, bus_pallete_rdata, gfx_pallete_addr;
     logic [31:0] gfx_pallete_rdata;
     logic  [3:0] bus_pallete_we;
-    logic        bus_pallete_valid, gfx_pallete_valid;
+    logic        bus_pallete_read, bus_pallete_write, gfx_pallete_read;
 
     logic [31:0] bus_oam_addr, bus_oam_rdata, gfx_oam_addr;
     logic [31:0] gfx_oam_rdata;
     logic  [3:0] bus_oam_we;
-    logic        bus_oam_valid, gfx_oam_valid;
+    logic        bus_oam_read, bus_oam_write, gfx_oam_read;
 
     logic  [3:0] bus_we;
 
@@ -85,25 +104,31 @@ module mem_top(
     assign bus_oam_addr = bus_mem_addr - `OAM_START;
     assign gfx_oam_addr = gfx_addr - `OAM_START;
 
-    /* Add reset, so that resets go through */
-    assign bus_system_valid = (bus_system_addr <= `SYSTEM_ROM_SIZE) | reset;
-    assign gfx_system_valid = (gfx_system_addr <= `SYSTEM_ROM_SIZE) | reset;
-    assign bus_intern_valid = (bus_intern_addr <= `INTERN_RAM_SIZE) | reset;
-    assign gfx_intern_valid = (gfx_intern_addr <= `INTERN_RAM_SIZE) | reset;
-    assign bus_vram_valid = (bus_vram_addr <= `VRAM_SIZE) | reset;
-    assign gfx_vram_valid = (gfx_vram_addr <= `VRAM_SIZE) | reset;
-    assign bus_pallete_valid = (bus_pallete_addr <= `PALLET_RAM_SIZE) | reset;
-    assign gfx_pallete_valid = (gfx_pallete_addr <= `PALLET_RAM_SIZE) | reset;
-    assign bus_oam_valid = (bus_oam_addr <= `OAM_SIZE) | reset;
-    assign gfx_oam_valid = (gfx_oam_addr <= `OAM_SIZE) | reset;
-    
-    assign bus_intern_we = (bus_intern_valid) ? bus_we : 4'd0;
-    assign bus_vram_we = (bus_vram_valid) ? bus_we : 4'd0;
-    assign bus_pallete_we = (bus_pallete_valid) ? bus_we : 4'd0;
-    assign bus_oam_we = (bus_oam_valid) ? bus_we : 4'd0;    
+    assign bus_system_read = bus_system_addr <= `SYSTEM_ROM_SIZE;
+    assign gfx_system_read = gfx_system_addr <= `SYSTEM_ROM_SIZE;
+
+    assign bus_intern_read = (bus_addr_lat1 - `INTERN_RAM_START) <= `INTERN_RAM_SIZE;
+    assign bus_intern_write = bus_intern_addr <= `INTERN_RAM_SIZE;
+    assign gfx_intern_read = gfx_intern_addr <= `INTERN_RAM_SIZE;
+
+    assign bus_vram_read = (bus_addr_lat1 - `VRAM_START) <= `VRAM_SIZE;
+    assign bus_vram_write = bus_vram_addr <= `VRAM_SIZE;
+    assign gfx_vram_read = gfx_vram_addr <= `VRAM_SIZE;
+
+    assign bus_pallete_read = (bus_addr_lat1 - `PALLET_RAM_START) <= `PALLET_RAM_SIZE;
+    assign bus_pallete_write = bus_pallete_addr <= `PALLET_RAM_SIZE;
+    assign gfx_pallete_read = gfx_pallete_addr <= `PALLET_RAM_SIZE;
+
+    assign bus_oam_read = (bus_addr_lat1 - `OAM_START) <= `OAM_SIZE;
+    assign bus_oam_write = bus_oam_addr <= `OAM_SIZE;
+    assign gfx_oam_read = gfx_oam_addr <= `OAM_SIZE;
+
+    assign bus_intern_we = (bus_intern_write) ? bus_we : 4'd0;
+    assign bus_vram_we = (bus_vram_write) ? bus_we : 4'd0;
+    assign bus_pallete_we = (bus_pallete_write) ? bus_we : 4'd0;
+    assign bus_oam_we = (bus_oam_write) ? bus_we : 4'd0;
 
 
-    // When enable is deasserted, no write/read/reset is performed
     // Data width set to 32bits, so addresses are aligned
     system_rom sys   (.clka(clock), .rsta(reset),
                       .addra({2'b0, bus_system_addr[31:2]}),
@@ -147,30 +172,30 @@ module mem_top(
 
 
     always_comb begin
-        if (bus_system_valid)
+        if (bus_system_read)
             bus_rdata = bus_system_rdata;
-        else if (bus_intern_valid)
+        else if (bus_intern_read)
             bus_rdata = bus_intern_rdata;
-        else if (bus_vram_valid)
+        else if (bus_vram_read)
             bus_rdata = bus_vram_rdata;
-        else if (bus_pallete_valid)
+        else if (bus_pallete_read)
             bus_rdata = bus_pallete_rdata;
-        else if (bus_oam_valid)
+        else if (bus_oam_read)
             bus_rdata = bus_oam_rdata;
         else
             bus_rdata = 32'hffffffff;
     end
 
     always_comb begin
-        if (gfx_system_valid)
+        if (gfx_system_read)
             gfx_data = gfx_system_rdata;
-        else if (gfx_intern_valid)
+        else if (gfx_intern_read)
             gfx_data = gfx_intern_rdata;
-        else if (gfx_vram_valid)
+        else if (gfx_vram_read)
             gfx_data = gfx_vram_rdata;
-        else if (gfx_pallete_valid)
+        else if (gfx_pallete_read)
             gfx_data = gfx_pallete_rdata;
-        else if (gfx_oam_valid)
+        else if (gfx_oam_read)
             gfx_data = gfx_oam_rdata;
         else
             gfx_data = 32'hffffffff;
@@ -207,3 +232,20 @@ module mem_decoder
      end
 
 endmodule: mem_decoder
+
+module register
+    #(parameter WIDTH = 8)
+    (input  logic clock, reset,
+     output logic [WIDTH-1:0] Q,
+     input  logic [WIDTH-1:0] D,
+     input  logic en, clr);
+
+    logic [WIDTH-1:0] D_next;
+    assign D_next = (clr) ? 0 : ((en) ? D : Q);
+
+    always_ff @(posedge clock, posedge reset) begin
+        if (reset) Q <= 0;
+        else Q <= D;
+    end
+
+endmodule: register
