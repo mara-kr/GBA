7af7b0d2752a76d780c1df970b913af3bfd6ec56
Neil D. Ryan <soctar@gmail.com>
2016-10-25 13:01:59 -0400

    Everything works

--- mem/mem_test.sv
+++ mem/mem_test.sv
@@ -1,7 +1,16 @@
+/* mem_test.sv
+ *
+ * Module to test memory controller for BRAM. Does NUM_TESTS consecutive
+ * writes then NUM_TESTS consecutive reads. Alternates half-words and
+ * words. Only tests the bus ports, since the interfaces to the BRAM
+ * are exposed.
+ *
+ * Neil Ryan
+ */
 `default_nettype none
 `include "core_tb_defines.vh"
 
-`define NUM_TESTS 12
+`define NUM_TESTS 13
 
 module mem_test_CI (
     input  logic GCLK, BTND,
@@ -11,35 +20,39 @@ module mem_test_CI (
     logic  [1:0] bus_size;
     logic        bus_pause, bus_write;
 
-    logic [31:0] gfx_addr, gfx_data;
-    logic  [1:0] gfx_size;
-    logic        gfx_pause;
+    logic [31:0] gfx_oam_addr, gfx_oam_data, gfx_vram_addr, gfx_vram_data;
+    logic [31:0] gfx_pallete_addr, gfx_pallete_data;
 
     logic [7:0] LD_next;
 
     mem_top mem (.clock(GCLK), .reset(BTND), .bus_wdata, .bus_rdata,
                  .bus_addr, .bus_size, .bus_pause, .bus_write,
-                 .gfx_addr, .gfx_data, .gfx_size, .gfx_pause);
+                 .gfx_pallete_addr, .gfx_pallete_data,
+                 .gfx_oam_addr, .gfx_oam_data,
+                 .gfx_vram_addr, .gfx_vram_data);
 
-    enum logic [1:0] {WADDR, WDATA, READ} cs, ns, cs_next;
-    logic [31:0] addrs [`NUM_TESTS-1:0];
-    logic [31:0] datas [`NUM_TESTS-1:0];
+    enum logic {WRITE, READ} cs, ns, cs_next;
+    logic [31:0] addrs [14:0];
+    logic [31:0] datas [14:0];
     logic [3:0] count, count_next;
     logic       en, clr, fail;
 
-    assign addrs = {32'h0700_0000, 32'h0500_0042, 32'h0700_03FF,
-                    32'h0600_0000, 32'h0300_1234, 32'h0601_7FFF,
-                    32'h0500_0000, 32'h0700_0042, 32'h0500_03FF,
+    assign addrs = {32'h0000_0000, 32'h0000_0000, 32'h0700_0225,
+                    32'h0600_0000, 32'h0500_0042, 32'h0700_03FF,
+                    32'h0500_0000, 32'h0300_1234, 32'h0601_7FFF,
+                    32'h0700_0000, 32'h0700_0042, 32'h0500_03FF,
                     32'h0300_0000, 32'h0600_1234, 32'h0300_7FFF};
 
-    assign datas = {32'hdead_beef, 32'hcafe_f00d, 32'hcafe_bebe,
+    assign datas = {32'h0000_0000, 32'h0000_0000, 32'hface_f00d,
+                    32'hdead_beef, 32'hcafe_f00d, 32'hcafe_bebe,
                     32'hbeef_bebe, 32'hbeef_f00d, 32'hdead_bebe,
                     32'hdead_f00d, 32'hba5e_ba11, 32'hcab0_05e5,
                     32'hc0ff_ee55, 32'he5ce_ca1d, 32'h5ece_de00};
 
-    assign bus_size = `MEM_SIZE_WORD;
-    assign gfx_size = `MEM_SIZE_WORD;
-    assign gfx_addr = 32'd0;
+    assign bus_size = count[0] ? `MEM_SIZE_WORD : `MEM_SIZE_HALF;
+    assign gfx_pallete_addr = 32'b0;
+    assign gfx_oam_addr = 32'b0;
+    assign gfx_vram_addr = 32'b0;
     assign cs_next = (~bus_pause) ? ns : cs;
 
     always_comb begin
@@ -53,14 +66,14 @@ module mem_test_CI (
     always_ff @(posedge GCLK, posedge BTND) begin
         if (BTND) begin
             count <= 4'd0;
-            cs <= WADDR;
+            cs <= WRITE;
         end else begin
             count <= count_next;
             cs <= cs_next;
         end
     end
 
-   assign LD_next = (fail) ? 8'hf0 : LD;
+   (* mark_debug = "true" *) assign LD_next = (fail) ? 8'hf0 : LD;
 
     always_ff @(posedge GCLK, posedge BTND) begin
         if (BTND) begin
@@ -70,34 +83,36 @@ module mem_test_CI (
         end
     end
 
-    assign bus_wdata = datas[count];
+    assign bus_wdata = datas[count-1];
     assign bus_addr = addrs[count];
-
-    logic [31:0] bus_rdata_old;
-    register #(32) bus_data (.clock(GCLK), .reset(BTND), .clr(1'b0),
-                             .en(en & ~bus_pause),
-                             .D(bus_wdata), .Q(bus_rdata_old));
 
     always_comb begin
         bus_write = 1'b0;
         en = 1'b0;
         fail = 1'b0;
         clr = 1'b0;
-        ns = WADDR;
+        ns = WRITE;
         case (cs)
-            WADDR: begin
-                bus_write = 1'b1;
-                ns = WDATA;
-            end
-            WDATA: begin
+            WRITE: begin
+                bus_write = (count != `NUM_TESTS);
+                ns = (count == `NUM_TESTS) ? READ : WRITE;
                 en = 1'b1;
-                ns = (count == `NUM_TESTS-1) ? READ : WADDR;
-                clr = (count == `NUM_TESTS-1);
+                clr = (count == `NUM_TESTS);
             end
             READ: begin
                 en = 1'b1;
-                fail = (bus_rdata != bus_rdata_old) & (count > 4'd0);
-                ns = (count == `NUM_TESTS) ? WADDR : READ;
+                if (~count[0]) begin // Wrote word
+                    fail = (bus_rdata != datas[count-1]) && (count > 4'd0);
+                end else begin
+                    if (addrs[count-1][1]) begin // Top half written to
+                        fail = (datas[count-1][31:16] != bus_rdata[31:16]) ||
+                               (bus_rdata[15:0] != 16'd0);
+                    end else begin // bottom half written to
+                        fail = (datas[count-1][15:0] != bus_rdata[15:0]) ||
+                                                   (bus_rdata[31:16] != 16'd0);
+                    end
+                end
+                ns = (count == `NUM_TESTS) ? WRITE : READ;
                 clr = (count == `NUM_TESTS);
             end
         endcase
@@ -124,4 +139,5 @@ module sim_top;
         #300 $finish;
     end
 endmodule: sim_top
+
 */
--- mem/mem_top.sv
+++ mem/mem_top.sv
@@ -14,12 +14,6 @@
  * Neil Ryan, <nryan@andrew.cmu.edu>
  */
 
-// TODO Test Consecutive Reads (DMA timing)
-// TODO Test Consecutive Writes (DMA timing)
-// TODO Test Reading from Graphics & CPU at once
-// TODO Test different size writes
-// TODO Test Pause on reads
-
 `default_nettype none
 `include "core_tb_defines.vh"
 
@@ -34,21 +28,17 @@ module mem_top(
     input  logic        bus_write,
 
     /* Signals for graphics Bus */
-    input  logic [31:0] gfx_addr,
-    output logic [31:0] gfx_data,
-    input  logic  [1:0] gfx_size,
-    output logic        gfx_pause
-
+    input  logic [31:0] gfx_oam_addr, gfx_pallete_addr, gfx_vram_addr,
+    output logic [31:0] gfx_oam_data, gfx_pallete_data, gfx_vram_data
     );
 
     /* Single cycle latency for writes */
-    (* mark_debug = "true" *) logic [31:0] bus_addr_lat1, bus_mem_addr, gfx_addr_lat1;
+    logic [31:0] bus_addr_lat1, bus_mem_addr, gfx_addr_lat1;
     logic  [1:0] bus_size_lat1;
     logic        bus_write_lat1;
 
     // Could add more pauses for memory regions, this is needed
     // because of the CPU's write format
-    assign gfx_pause = 1'b0;
 
     // Use delayed memory address on writes
     assign bus_mem_addr = (bus_write_lat1) ? bus_addr_lat1 : bus_addr;
@@ -64,29 +54,24 @@ module mem_top(
     register #(1) wpause (.clock, .reset, .en(1'b1), .clr(1'b0),
                          .D(bus_write & ~bus_pause), .Q(bus_pause));
 
-    logic [31:0] bus_system_addr, bus_system_rdata, gfx_system_addr;
-    logic [31:0] gfx_system_rdata;
-    (* mark_debug = "true" *) logic        bus_system_read, gfx_system_read;
+    logic [31:0] bus_system_addr, bus_system_rdata;
+    logic        bus_system_read;
 
-    logic [31:0] bus_intern_addr, bus_intern_rdata, gfx_intern_addr;
-    logic [31:0] gfx_intern_rdata;
+    logic [31:0] bus_intern_addr, bus_intern_rdata;
     logic  [3:0] bus_intern_we;
-    (* mark_debug = "true" *) logic        bus_intern_read, bus_intern_write, gfx_intern_read;
+    logic        bus_intern_read, bus_intern_write;
 
-    logic [31:0] bus_vram_addr, bus_vram_rdata, gfx_vram_addr;
-    logic [31:0] gfx_vram_rdata;
+    logic [31:0] bus_vram_addr, bus_vram_rdata;
     logic  [3:0] bus_vram_we;
-    (* mark_debug = "true" *) logic        bus_vram_read, bus_vram_write, gfx_vram_read;
+    logic        bus_vram_read, bus_vram_write;
 
-    logic [31:0] bus_pallete_addr, bus_pallete_rdata, gfx_pallete_addr;
-    logic [31:0] gfx_pallete_rdata;
+    logic [31:0] bus_pallete_addr, bus_pallete_rdata;
     logic  [3:0] bus_pallete_we;
-    (* mark_debug = "true" *) logic        bus_pallete_read, bus_pallete_write, gfx_pallete_read;
+    logic        bus_pallete_read, bus_pallete_write;
 
-    logic [31:0] bus_oam_addr, bus_oam_rdata, gfx_oam_addr;
-    logic [31:0] gfx_oam_rdata;
+    logic [31:0] bus_oam_addr, bus_oam_rdata;
     logic  [3:0] bus_oam_we;
-    (* mark_debug = "true" *) logic        bus_oam_read, bus_oam_write, gfx_oam_read;
+    logic        bus_oam_read, bus_oam_write;
 
     logic  [3:0] bus_we;
 
@@ -94,40 +79,29 @@ module mem_top(
                          .write(bus_write_lat1), .byte_we(bus_we));
 
     assign bus_system_addr = bus_mem_addr;
-    assign gfx_system_addr = gfx_addr;
     assign bus_intern_addr = bus_mem_addr - `INTERN_RAM_START;
-    assign gfx_intern_addr = gfx_addr - `INTERN_RAM_START;
     assign bus_vram_addr = bus_mem_addr - `VRAM_START;
-    assign gfx_vram_addr = gfx_addr - `VRAM_START;
     assign bus_pallete_addr = bus_mem_addr - `PALLET_RAM_START;
-    assign gfx_pallete_addr = gfx_addr - `PALLET_RAM_START;
     assign bus_oam_addr = bus_mem_addr - `OAM_START;
-    assign gfx_oam_addr = gfx_addr - `OAM_START;
 
     assign bus_system_read = bus_addr_lat1 <= `SYSTEM_ROM_SIZE;
-    assign gfx_system_read = gfx_system_addr <= `SYSTEM_ROM_SIZE;
 
     assign bus_intern_read = (bus_addr_lat1 - `INTERN_RAM_START) <= `INTERN_RAM_SIZE;
     assign bus_intern_write = bus_intern_addr <= `INTERN_RAM_SIZE;
-    assign gfx_intern_read = gfx_intern_addr <= `INTERN_RAM_SIZE;
 
     assign bus_vram_read = (bus_addr_lat1 - `VRAM_START) <= `VRAM_SIZE;
     assign bus_vram_write = bus_vram_addr <= `VRAM_SIZE;
-    assign gfx_vram_read = gfx_vram_addr <= `VRAM_SIZE;
 
     assign bus_pallete_read = (bus_addr_lat1 - `PALLET_RAM_START) <= `PALLET_RAM_SIZE;
     assign bus_pallete_write = bus_pallete_addr <= `PALLET_RAM_SIZE;
-    assign gfx_pallete_read = gfx_pallete_addr <= `PALLET_RAM_SIZE;
 
     assign bus_oam_read = (bus_addr_lat1 - `OAM_START) <= `OAM_SIZE;
     assign bus_oam_write = bus_oam_addr <= `OAM_SIZE;
-    assign gfx_oam_read = gfx_oam_addr <= `OAM_SIZE;
 
     assign bus_intern_we = (bus_intern_write) ? bus_we : 4'd0;
     assign bus_vram_we = (bus_vram_write) ? bus_we : 4'd0;
     assign bus_pallete_we = (bus_pallete_write) ? bus_we : 4'd0;
     assign bus_oam_we = (bus_oam_write) ? bus_we : 4'd0;
-
 
     // Data width set to 32bits, so addresses are aligned
     system_rom sys   (.clka(clock), .rsta(reset),
@@ -135,16 +109,16 @@ module mem_top(
                       .douta(bus_system_rdata),
 
                       .clkb(clock), .rstb(reset),
-                      .addrb({2'b0, gfx_system_addr[31:2]}),
-                      .doutb(gfx_system_rdata));
+                      .addrb(32'b0),
+                      .doutb());
 
     InternRAM intern (.clka(clock), .rsta(reset),
                       .wea(bus_intern_we), .addra({2'b0, bus_intern_addr[31:2]}),
                       .douta(bus_intern_rdata), .dina(bus_wdata),
 
                       .clkb(clock), .rstb(reset),
-                      .web(4'd0), .addrb({2'b0, gfx_intern_addr[31:2]}),
-                      .doutb(gfx_intern_rdata), .dinb(32'b0));
+                      .web(4'd0), .addrb(32'b0),
+                      .doutb(), .dinb(32'b0));
 
     vram vram_mem    (.clka(clock), .rsta(reset),
                       .wea(bus_vram_we), .addra({2'b0, bus_vram_addr[31:2]}),
@@ -152,7 +126,7 @@ module mem_top(
 
                       .clkb(clock), .rstb(reset),
                       .web(4'd0), .addrb({2'b0, gfx_vram_addr[31:2]}),
-                      .doutb(gfx_vram_rdata), .dinb(32'b0));
+                      .doutb(gfx_vram_data), .dinb(32'b0));
 
     palette_ram pall (.clka(clock), .rsta(reset),
                       .wea(bus_pallete_we), .addra({2'b0, bus_pallete_addr[31:2]}),
@@ -160,7 +134,7 @@ module mem_top(
 
                       .clkb(clock), .rstb(reset),
                       .web(4'd0), .addrb({2'b0, gfx_pallete_addr[31:2]}),
-                      .doutb(gfx_pallete_rdata), .dinb(32'b0));
+                      .doutb(gfx_pallete_data), .dinb(32'b0));
 
     OAM oam_mem      (.clka(clock), .rsta(reset),
                       .wea(bus_oam_we), .addra({2'b0, bus_oam_addr[31:2]}),
@@ -168,7 +142,7 @@ module mem_top(
 
                       .clkb(clock), .rstb(reset),
                       .web(4'd0), .addrb({2'b0, gfx_pallete_addr[31:2]}),
-                      .doutb(gfx_oam_rdata), .dinb(32'b0));
+                      .doutb(gfx_oam_data), .dinb(32'b0));
 
 
     always_comb begin
@@ -186,21 +160,6 @@ module mem_top(
             bus_rdata = 32'hz;
     end
 
-    // TODO should be muxing on addr delayed by 1 cycle
-    always_comb begin
-        if (gfx_system_read)
-            gfx_data = gfx_system_rdata;
-        else if (gfx_intern_read)
-            gfx_data = gfx_intern_rdata;
-        else if (gfx_vram_read)
-            gfx_data = gfx_vram_rdata;
-        else if (gfx_pallete_read)
-            gfx_data = gfx_pallete_rdata;
-        else if (gfx_oam_read)
-            gfx_data = gfx_oam_rdata;
-        else
-            gfx_data = 32'hz;
-    end
 endmodule: mem_top
 
 /* Setup byte write enables for memory (assumes that CPU deals with
