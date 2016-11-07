7d3a6dd100ba3cbcd6f233633fada6512b358ba0
Steven Kool <skool@andrew.cmu.edu>
2016-11-06 20:51:06 -0500

    BG Processing circuit modules are code complete. Still need to write top-level module for BG processing.

--- graphics/bg_processing_circuit/data_formatter.sv
+++ graphics/bg_processing_circuit/data_formatter.sv
@@ -1,11 +1,12 @@
 module data_formatter
   (input logic [14:0] data,
    input logic x,
-   input logic palettemode, bitmapped,
+   input logic palettemode,
    input logic bitmapped, transparent,
    input logic [3:0] paletteno,
    input logic [1:0] bgno,
    input logic [1:0] bg_priority,
+   input logic bgused,
    output logic [19:0] formatted);
 
   logic [7:0] palette_index;
@@ -15,17 +16,17 @@ module data_formatter
   logic [7:0] palette_byte;
 
   assign palette_byte[7:4] = 4'b0;
-  graphics_mux_2_to_1 #(4) small_palette_mux(.I0(data[3:0]), .I1(data[7:4]), .S(x), .Y(palette_byte[3:0]));
-  graphics_mux_2_to_1 #(8) palette_mux(.I0(palette_byte), .I1(data[7:0]), .S(palettemode), .Y(palette_index));
+  bg_mux_2_to_1 #(4) small_palette_mux(.i0(data[3:0]), .i1(data[7:4]), .s(x), .y(palette_byte[3:0]));
+  bg_mux_2_to_1 #(8) palette_mux(.i0(palette_byte), .i1(data[7:0]), .s(palettemode), .y(palette_index));
 
   assign visible = (|palette_index | bitmapped) & bgused & ~transparent;
 
-  graphics_mux_2_to_1 #(8) pinfo_mux(.IO({paletteno, paletteindex[3:0]}), .I1(paletteindex), .S(palettemode), .Y(pinfo));
+  bg_mux_2_to_1 #(8) pinfo_mux(.i0({paletteno, palette_index[3:0]}), .i1(palette_index), .s(palettemode), .y(pinfo));
 
   logic [14:0] bitmapped_data, char_data, payload;
   assign char_data = {4'b0, bgno, 1'b0, pinfo};
   assign bitmapped_data = data;
-  graphics_mux_2_to_1 #(15) payload_mux(.I0(char_data), .I1(bitmapped_data), .S(bitmapped), .Y(payload));
+  bg_mux_2_to_1 #(15) payload_mux(.i0(char_data), .i1(bitmapped_data), .s(bitmapped), .y(payload));
 
   assign formatted = {bg_priority, 1'b0, bitmapped, visible, payload};
 
