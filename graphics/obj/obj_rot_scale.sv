4e9961ee4452f1f842342a0f74d7a10afa9b8cc1
Steven Kool <skool@andrew.cmu.edu>
2016-11-07 11:29:54 -0500

    Fixes to obj_rot_scale unit. Added transparent output.

--- graphics/obj/obj_rot_scale.sv
+++ graphics/obj/obj_rot_scale.sv
@@ -5,14 +5,15 @@ module obj_rot_scale_unit
    input logic [7:0] obj_y,
    input logic [7:0] hsize, vsize,
    input logic double_size,
-   output logic [5:0] x, y);
+   output logic [5:0] x, y,
+   output logic transparent);
 
   //distances from reference point of rotation (obj center)
   logic [6:0] dx;
   logic [6:0] dy;
 
   assign dx = col - obj_x;
-  assign dy = col - obj_y;
+  assign dy = row - obj_y;
 
   //justify results into the range [0, 64)
   logic [5:0] x_bias;
@@ -22,7 +23,13 @@ module obj_rot_scale_unit
   assign y_bias = double_size ? vsize[7:2] : vsize[6:1]; 
 
   //math
-  assign x = a * dx + b * dy + x_bias; 
-  assign y = c * dx + d * dy + y_bias;
+  logic [22:0] product_x, product_y;
+  assign product_x = a * dx + b * dy + {x_bias, 8'b0}; 
+  assign product_y = c * dx + d * dy + {y_bias, 8'b0};
+
+  assign x = product_x[13:8];
+  assign y = product_y[13:8];
+
+  assign transparent = |product_x[22:14] | |product_y[22:14];
 
 endmodule: obj_rot_scale_unit
