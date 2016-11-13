05c008e392c6271ed173cc930b713a356f65e7d3
Steven Kool <skool@andrew.cmu.edu>
2016-11-13 13:35:21 -0500

    Manual test pattern for graphics

--- graphics/manual_testbench.sv
+++ graphics/manual_testbench.sv
@@ -41,7 +41,7 @@ module manual_bench (
     logic [15:0] bg3pd;
     logic [15:0] mosaic;
 
-    input logic [31:0] IO_reg_datas [`NUM_IO_REGS-1:0],
+    logic [31:0] IO_reg_datas [`NUM_IO_REGS-1:0];
 
     logic clock, reset;
 
@@ -144,9 +144,146 @@ module manual_bench (
 
     //default BG2 to no rotation
     //rotate 45 degrees if SW[6] is set 
-    assign bg2pa = SW[6] ? : 16'h005A;
-    assign bg2pb = SW[6] ? : 16'h805A;
-    assign bg2pc = SW[6] ? : 16'h005A;
-    assign bg2pd = SW[6] ? : 16'h005A;
+    assign bg2pa = SW[6] ? 16'h005A : 16'h0100;
+    assign bg2pb = SW[6] ? 16'h805A : 16'h0000;
+    assign bg2pc = SW[6] ? 16'h005A : 16'h0000;
+    assign bg2pd = SW[6] ? 16'h005A : 16'h0100;
+    assign bg3pa = 16'h0100;
+    assign bg3pb = 16'h0000;
+    assign bg3pc = 16'h0000;
+    assign bg3pd = 16'h0100;
+
+    //window 0 covers left third of the screen
+    assign win0h = 16'h0050;
+    assign win0v = 16'h00A0;
+
+    //window 1 covers right third of the screen
+    assign win1h = 16'hA0F0;
+    assign win1v = 16'h00A0;
+
+    //enable display in win0 if BTNL is pressed, in win1 if BTNR
+    assign winin = {2'b0, {6{BTNR}}, 2'b0, {6{BTNL}}};
+    assign winout = 16'hFFFF; //always display outside of windows
+
+    //disable special effects
+    assign bldcnt = 16'h0000;
+
+    //special effects will do.. something?
+    assign bldalpha = 16'h001F;
+    assign bldy = 16'h001F;
+
+    //module instantiations
+    logic wen, toggle;
+    logic graphics_clock, vga_block;
+    logic [14:0] graphics_color, vga_color;
+    logic [16:0] graphics_addr, vga_addr;
+   
+    logic [14:0] buffer0_dout, buffer1_dout;
+    logic [16:0] buffer0_address, buffer1_address;
+    logic [14:0] buffer0_din, buffer1_din;
+    logic buffer0_ce, buffer1_ce;
+    logic buffer0_we, buffer1_we;
+    //clock wiz
+    design_1_clk_wiz_0_1 clk_wiz(.clk_in1(GCLK), .clk_out1(vga_clock), .clk_out2(graphics_clock), .reset(BTND));
+
+    //dbl_buffer buffers
+    testbench_wrapper brams(.buffer0_addr(buffer0_address), .buffer0_clk(vga_clock), .buffer0_din, .buffer0_dout, .buffer0_en(buffer0_ce), .buffer0_we,
+                            .buffer1_addr(buffer1_address), .buffer1_clk(vga_clock), .buffer1_din, .buffer1_dout, .buffer1_en(buffer1_ce), .buffer1_we);
+
+    //interface between graphics and dbl_buffer
+    dblbuffer_driver driver(.toggle, .wen, .graphics_clock, .vcount, .graphics_addr, .clk(vga_clock), .rst_b(~BTND));
+
+    //double_buffer
+    double_buffer video_buf(.ap_clk(vga_clock), .ap_rst_n(~BTND), .graphics_addr, .graphics_color, .toggle, .vga_addr, .wen, .vga_color,
+                            .buffer0_address, .buffer1_address,
+                            .buffer0_din, .buffer1_din,
+                            .buffer0_dout, .buffer1_dout,
+                            .buffer0_ce, .buffer1_ce,
+                            .buffer0_we, .buffer1_we);
+    //vga
+    vga_top video(.clock(vga_clock), .reset(BTND), .data(vga_color), .addr(vga_addr), .VGA_R, .VGA_G, .VGA_B, .VGA_HS, .VGA_VS);
+
+    //graphics
+    graphics_top(.clock(graphics_clock), .reset, .IO_reg_datas,
+                 .gfx_vram_A_data, .gfx_vram_B_data, .gfx_vram_C_data,
+                 .gfx_oam_data, .gfx_palette_bg_data, .gfx_pallete_obj_data,
+                 .gfx_vram_A_data2m,
+                 .gfx_vram_A_addr, .gfx_vram_B_addr, .gfx_vram_C_addr,
+                 .gfx_oam_addr, .gfx_palette_bg_addr, .gfx_palette_obj_addr,
+                 .gfx_vram_A_addr2,
+                 .output_color(graphics_color));
 
 endmodule: manual_bench
+
+module dblbuffer_driver(
+    output logic toggle,
+    output logic wen,
+    input logic graphics_clock,
+    output logic [16:0] graphics_addr,
+    output logic [7:0] vcount,
+    input logic clk,
+    input logic rst_b
+    );
+    
+    assign vcount = row;
+    
+    dbdriver_counter #(20, 842687) toggler(.clk, .rst_b, .en(1'b1), .last(toggle), .Q());
+    
+    logic [18:0] addr;
+    logic [7:0] row;
+    logic [8:0] col;
+    
+    logic step_row;
+    logic next_frame;
+    assign graphics_addr = addr[18:2];
+    dbdriver_counter #(19, 280895) addrs(.clk(graphics_clock), .en(1'b1), .clear(1'b0), .rst_b, .last(next_frame), .Q(addr));
+
+    dbdriver_counter #(8, 159) rows(.clk(graphics_clock), .en(step_row & addr[1] & addr[0]), .clear(next_frame & addr[1] & addr[0]), .rst_b, .last(), .Q(row));
+    dbdriver_counter #(9, 239) cols(.clk(graphics_clock), .en(addr[1] & addr[0]), .clear(next_frame & addr[1] & addr[0]), .rst_b, .last(step_row), .Q(col));
+
+    assign wen = graphics_addr < 38400;
+    
+endmodule
+
+module dbdriver_counter 
+    #(
+    parameter WIDTH=18,
+    parameter MAX = 210671
+    )
+    (
+    input logic clk,
+    input logic rst_b,
+    input logic en,
+    input logic clear,
+    output logic [WIDTH-1:0] Q,
+    output logic last
+    );
+    
+    assign last = Q == MAX;
+    
+    logic [WIDTH-1:0] next;
+        
+    always_comb begin
+        if(clear) begin
+            next = 0;    
+        end
+        else if(~en) begin
+            next = Q;
+        end
+        else if(last) begin
+            next = 0;
+        end
+        else begin
+            next = Q + 1;
+        end
+    end 
+
+    always_ff @(posedge clk, negedge rst_b) begin
+        if(~rst_b) begin
+            Q <= 0;
+        end
+        else begin
+            Q <= next;
+        end
+    end
+endmodule
