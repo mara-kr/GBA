c835bac4b0f6e23e3692d030ad0803588008e4e1
ryanovsky <ryanovsky@users.noreply.github.com>
2016-11-21 10:48:31 -0500

    Audio (#6)
    * working zync output to audio codec
    
    * updated readme for link on where code came from
    
    * begin creating building block modules
    
    * update .gitignore
    
    * added frequency timer and square wave modules
    
    * removing binaries and swp files
    
    * untested noise channel
    
    * added testbench and fixed bugs in noise channel
    
    * added frequency sweep module
    
    * can't remember what change i made to the square wave
    
    * finished wave channel and basic test
    
    * change variable name
    
    * initial combining to make 4 channels
    
    * added volume control to wave channel
    
    * remove errors and warnings when compiling
    
    * change all output bits to 4 bits
    
    * actually removed all warnings and errors now
    
    * add verilog testbench
    
    * Audio working with sv testbench: `default_nettype none saves lives.
    
    * started adding mixer
    
    * 4channels on fpga with bugs
    
    * working square 2 and noise
    
    * wave channel working
    
    * made random changes to square wave, going to get rid of inout NR13 now
    
    * frequency sweep outputs a square wave, but still no sweep
    
    * added clock generator, receive no timing warnings, but not tested
    
    * tested frequency is correct, and duty cycle
    
    * clean git repo-add to gitignore, remove vivado files
    
    * added clock divider and changed frequency sweep, tested only in simulation
    
    * fixed testbench
    
    * add volume envelope, only tested in simulation
    
    * works! about 80% tested
    
    * forgot to push this earlier
    
    * starting direct sound
    
    * a single timer
    
    * changed name of 4channel mixer
    
    * need to add count_up
    
    * added timer as input
    
    * added overall mixer for entire system
    
    * rerganizing branch
    
    * working zync output to audio codec
    
    * updated readme for link on where code came from
    
    * begin creating building block modules
    
    * update .gitignore
    
    * added frequency timer and square wave modules
    
    * removing binaries and swp files
    
    * untested noise channel
    
    * added testbench and fixed bugs in noise channel
    
    * added frequency sweep module
    
    * can't remember what change i made to the square wave
    
    * finished wave channel and basic test
    
    * change variable name
    
    * initial combining to make 4 channels
    
    * added volume control to wave channel
    
    * remove errors and warnings when compiling
    
    * change all output bits to 4 bits
    
    * actually removed all warnings and errors now
    
    * add verilog testbench
    
    * Audio working with sv testbench: `default_nettype none saves lives.
    
    * started adding mixer
    
    * 4channels on fpga with bugs
    
    * working square 2 and noise
    
    * wave channel working
    
    * made random changes to square wave, going to get rid of inout NR13 now
    
    * frequency sweep outputs a square wave, but still no sweep
    
    * added clock generator, receive no timing warnings, but not tested
    
    * tested frequency is correct, and duty cycle
    
    * clean git repo-add to gitignore, remove vivado files
    
    * added clock divider and changed frequency sweep, tested only in simulation
    
    * fixed testbench
    
    * add volume envelope, only tested in simulation
    
    * works! about 80% tested
    
    * forgot to push this earlier
    
    * starting direct sound
    
    * a single timer
    
    * changed name of 4channel mixer
    
    * need to add count_up
    
    * added timer as input
    
    * added overall mixer for entire system
    
    * rerganizing branch
    
    * fixed syntax erros
    
    * added top and count_up functionality
    
    * added dma sound to top
    
    * Delected files in 4channel & direct-sound
    
    * remove repeated files from reorganization
    
    * change pull request errors

---
+++ timer/timer_top.sv
@@ -0,0 +1,64 @@
+`default_nettype none
+`include "../gba_mmio_defines.vh"
+`include "../gba_core_defines.vh"
+
+module timer_top (
+    input logic clock_16,
+    input logic reset,
+    input logic [31:0] IO_reg_datas [`NUM_IO_REGS-1:0],
+    output logic genIRQ0,
+    output logic genIRQ1,
+    output logic genIRQ2,
+    output logic genIRQ3);
+
+    wire [15:0] TM0CNT_L, TM1CNT_L;
+    wire [15:0] TM2CNT_L, TM3CNT_L;
+    logic [15:0] TM0CNT_H, TM1CNT_H;
+    logic [15:0] TM2CNT_H, TM3CNT_H;
+
+    assign TM0CNT_L = IO_reg_datas[`TM0CNT_L_IDX][15:0];
+    assign TM1CNT_L = IO_reg_datas[`TM1CNT_L_IDX][15:0];
+    assign TM2CNT_L = IO_reg_datas[`TM2CNT_L_IDX][15:0];
+    assign TM3CNT_L = IO_reg_datas[`TM3CNT_L_IDX][15:0];
+
+    assign TM0CNT_H = IO_reg_datas[`TM0CNT_L_IDX][31:16];
+    assign TM1CNT_H = IO_reg_datas[`TM1CNT_L_IDX][31:16];
+    assign TM2CNT_H = IO_reg_datas[`TM2CNT_L_IDX][31:16];
+    assign TM3CNT_H = IO_reg_datas[`TM3CNT_L_IDX][31:16];
+
+    timer timer0(
+        .clock_16,
+        .reset,
+        .TMxCNT_L(TM0CNT_L),
+        .TMxCNT_H(TM0CNT_H),
+        .genIRQ(genIRQ0),
+        .prev_timer(16'hFF));
+
+     timer timer1(
+        .clock_16,
+        .reset,
+        .TMxCNT_L(TM1CNT_L),
+        .TMxCNT_H(TM1CNT_H),
+        .genIRQ(genIRQ1),
+        .prev_timer(TM0CNT_L));
+
+     timer timer2(
+        .clock_16,
+        .reset,
+        .TMxCNT_L(TM2CNT_L),
+        .TMxCNT_H(TM2CNT_H),
+        .genIRQ(genIRQ2),
+        .prev_timer(TM1CNT_L));
+    
+     timer timer3(
+        .clock_16,
+        .reset,
+        .TMxCNT_L(TM3CNT_L),
+        .TMxCNT_H(TM3CNT_H),
+        .genIRQ(genIRQ3),
+        .prev_timer(TM2CNT_L));
+
+endmodule: timer_top
+
+`default_nettype wire
+
